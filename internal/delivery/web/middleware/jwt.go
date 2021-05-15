077c3a61aa99d521e24fabc781e43c9e2ca6e6ec
Arda Firdaus Ramadhan <ardafirdausr@gmail.com>
2021-05-15 19:18:13 +0700

    remove bug

--- internal/delivery/web/middleware/jwt.go
+++ internal/delivery/web/middleware/jwt.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/ardafirdausr/todo-server/internal/entity"
@@ -9,10 +10,20 @@ import (
 )
 
 func JWT() echo.MiddlewareFunc {
+
 	JWTSecretKey := os.Getenv("JWT_SECRET_KEY")
+
 	config := middleware.JWTConfig{
-		Claims:     &entity.JWTPayload{},
-		SigningKey: []byte(JWTSecretKey),
+		Claims:       &entity.JWTPayload{},
+		SigningKey:   []byte(JWTSecretKey),
+		ErrorHandler: customJWTErrorHandler,
 	}
 	return middleware.JWTWithConfig(config)
+}
+
+func customJWTErrorHandler(err error) error {
+	return &echo.HTTPError{
+		Code:    http.StatusUnauthorized,
+		Message: "Unauthorized",
+	}
 }
